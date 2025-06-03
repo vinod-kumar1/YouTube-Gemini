bee575350c3957fa136d3bfe42cbda89a0014553
vinod-kumar1 <vinu22149@gmail.com>
2025-06-03 20:36:59 +0530

    cleared git cache

--- src/utils/appStore.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import videoStore from "./videoStore";
-
-let appStore = configureStore({
-  reducer: {
-    videos: videoStore,
-  },
-});
-export default appStore;
