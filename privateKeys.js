7cd4206efda1ab92829251f560b6a992328415ed
vinod-kumar1 <vinu22149@gmail.com>
2025-06-07 11:21:31 +0530

    ...

---
+++ privateKeys.js
@@ -0,0 +1,10 @@
+export let privateKey = {
+  options: {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMDZmZGUxYmM4ZTljNmI4NzlmZDM4MDE4MDU3ZDllNyIsIm5iZiI6MTc0ODI3NTYyNS41NzMsInN1YiI6IjY4MzQ5MWE5ZDhjYTc0YTlkYWI2YjdkOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.FNrkauzLe3hrUAtXB3DnnhDDMUFu0wSMX9ikNciDfc8",
+    },
+  },
+};
